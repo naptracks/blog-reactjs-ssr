beaf5e7e1c2c950f3c262c916069fb9ad8e06a3c
naptracks <cesar.martel9@gmail.com>
2022-02-24 22:38:38 +0100

    Home page styles: App.js in progress

--- src/client/components/layout/Layout.js
+++ src/client/components/layout/Layout.js
@@ -1,15 +1,16 @@
 import React from "react";
 import Navbar from "../ui/Navbar";
 import Footer from "../ui/Footer";
+import Aux from "../../utils/Aux";
 
 const Layout = (props) => {
 
     return (
-        <>
+        <Aux>
             <Navbar/>
             {props.children}
             <Footer/>
-        </>
+        </Aux>
     )
 };
 
--- src/client/components/ui/Navbar.js
+++ src/client/components/ui/Navbar.js
@@ -2,14 +2,20 @@ import React from "react";
 
 const Navbar = () => {
 
-    return (
-        <div className={'navbar'}>
-            <p>Search</p>
-            <h1>Logo</h1>
-            <p>other</p>
-            <div>
+    const fakeLinks = ['new', 'for him', 'for her', 'home', 'gifts', 'sale', 'journal']
 
+    return (
+        <div className={'navbar-container'}>
+            <div className={'navbar center'}>
+                <h4>SEARCH</h4>
+                <img src={'logo.png'} alt={'logo'} />
+                <h4>OTHER</h4>
             </div>
+                <div className={'fake-links-container center'}>
+                    {
+                        fakeLinks.map(fl => <h4 className="fake-link">{fl.toUpperCase()}</h4>)
+                    }
+                </div>
         </div>
     )
 }
